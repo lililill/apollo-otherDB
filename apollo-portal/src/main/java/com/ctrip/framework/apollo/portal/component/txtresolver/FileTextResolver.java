6aa339cc79bd709166ee57d04553ee35f41714bb
Jason Song <nobodyiam@gmail.com>
2019-06-07 22:00:22 +0800

    fix the issue that a non-properties namespace could not be cleared (#2320)

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/component/txtresolver/FileTextResolver.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/component/txtresolver/FileTextResolver.java
@@ -17,7 +17,7 @@ public class FileTextResolver implements ConfigTextResolver {
   @Override
   public ItemChangeSets resolve(long namespaceId, String configText, List<ItemDTO> baseItems) {
     ItemChangeSets changeSets = new ItemChangeSets();
-    if (StringUtils.isEmpty(configText)) {
+    if (CollectionUtils.isEmpty(baseItems) && StringUtils.isEmpty(configText)) {
       return changeSets;
     }
     if (CollectionUtils.isEmpty(baseItems)) {
