46ed5271b1f4237a8cc065e957723674f4b0c958
xzxiaoshan <365384722@qq.com>
2019-06-15 16:23:12 +0800

    support setting server port from enviroment variables
    1.将服务端口提取到docker run的 -e SERVER_PORT 可指定，在docker需要使用host网络时，可以不用修改代码指定端口，避免与宿主机上的原有端口占用冲突 (#2333)
    
    2.对docker CMD入口的startup.sh文件，增加可执行授权指令，防止在某些时候该文件被打包进入后没有执行权限的麻烦（已实际遇到，又回头重新打包）

--- apollo-adminservice/src/main/scripts/startup.sh
+++ apollo-adminservice/src/main/scripts/startup.sh
@@ -3,7 +3,8 @@ SERVICE_NAME=apollo-adminservice
 ## Adjust log dir if necessary
 LOG_DIR=/opt/logs/100003172
 ## Adjust server port if necessary
-SERVER_PORT=8090
+#SERVER_PORT=8090
+SERVER_PORT=${SERVER_PORT:=8090}
 
 ## Create log directory if not existed because JDK 8+ won't do that
 mkdir -p $LOG_DIR
