21ac89c5dac8cbad087aaa1a54825022091f4b41
anilople <15523186+Anilople@users.noreply.github.com>
2021-05-08 22:03:21 +0800

    test(apollo-client): use assertEquals instead of assertThat

--- apollo-client/src/test/java/com/ctrip/framework/apollo/internals/LocalFileConfigRepositoryTest.java
+++ apollo-client/src/test/java/com/ctrip/framework/apollo/internals/LocalFileConfigRepositoryTest.java
@@ -1,8 +1,6 @@
 package com.ctrip.framework.apollo.internals;
 
-import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -138,9 +136,9 @@ public class LocalFileConfigRepositoryTest {
 
     Properties result = localFileConfigRepository.getConfig();
 
-    assertThat(
+    assertEquals(
         "LocalFileConfigRepository's properties should be the same as fallback repo's when there is no local cache",
-        result.entrySet(), equalTo(someProperties.entrySet()));
+        result, someProperties);
     assertEquals(someSourceType, localFileConfigRepository.getSourceType());
   }
 
@@ -159,9 +157,9 @@ public class LocalFileConfigRepositoryTest {
 
     Properties anotherProperties = anotherLocalRepoWithNoFallback.getConfig();
 
-    assertThat(
+    assertEquals(
         "LocalFileConfigRepository should persist local cache files and return that afterwards",
-        someProperties.entrySet(), equalTo(anotherProperties.entrySet()));
+        someProperties, anotherProperties);
     assertEquals(someSourceType, localRepo.getSourceType());
   }
 
