a6c7aa62c631c7ff3ce482d05b3d4c213090f261
kl <chenkailing@xd.com>
2023-03-14 09:56:35 +0800

    Optimising ServiceRegistry creation SQL (#4792)
    * add tech-support-qq-4.png
    
    * Update README.md
    
    * Enhance the user experience in the scenario of submitting duplicate keys
    
    * Modify the key-value conflict exception prompt, adjust the code style
    
    * chore(apollo-scripts): Optimising the creation of ServiceRegistry SQL
    
    * chore(apollo-scripts): Optimising the creation of ServiceRegistry SQL
    
    ---------
    
    Co-authored-by: Jason Song <nobodyiam@gmail.com>

--- scripts/sql/delta/v200-v210/apolloconfigdb-v200-v210.sql
+++ scripts/sql/delta/v200-v210/apolloconfigdb-v200-v210.sql
@@ -29,8 +29,8 @@ CREATE TABLE `ServiceRegistry` (
     `Uri`                    VARCHAR(64)   NOT NULL COMMENT '服务地址',
     `Cluster`                VARCHAR(64)   NOT NULL COMMENT '集群，可以用来标识apollo.cluster或者网络分区',
     `Metadata`               VARCHAR(1024) NOT NULL DEFAULT '{}' COMMENT '元数据，key value结构的json object，为了方面后面扩展功能而不需要修改表结构',
-    `DataChange_CreatedTime` TIMESTAMP     NOT NULL COMMENT '创建时间',
-    `DataChange_LastTime`    TIMESTAMP     NOT NULL COMMENT '最后修改时间',
+    `DataChange_CreatedTime` TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `DataChange_LastTime`    TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
     PRIMARY KEY (`Id`),
     UNIQUE INDEX `IX_UNIQUE_KEY` (`ServiceName`, `Uri`),
     INDEX                    `IX_DataChange_LastTime` (`DataChange_LastTime`)
