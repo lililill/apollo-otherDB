6657a58831a544606f78177b61d92bb959ccce94
ZhangJian He <shoothzj@gmail.com>
2024-01-01 21:20:53 +0800

    Change Copy Right year to 2024
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,5 +1,5 @@
 rem
-rem Copyright 2023 Apollo Authors
+rem Copyright 2024 Apollo Authors
 rem
 rem Licensed under the Apache License, Version 2.0 (the "License");
 rem you may not use this file except in compliance with the License.
