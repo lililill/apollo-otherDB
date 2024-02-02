1e03c819276d527a4129dd6350a83e97228af59a
liguiping <ligp@tomtaw.com.cn>
2024-02-02 15:01:57 +0800

    修改驱动

---
+++ build.sh
@@ -0,0 +1,61 @@
+mvn install:install-file -Dfile=apollo-common/libs/hibernate-4.dialect.jar -DgroupId=com.kingbase8.jdbc  -DartifactId=kingbase8 -Dversion=8.6.0 -Dpackaging=jar
+
+./scripts/build.sh 
+mkdir ./docker_build/apollo-adminservice/apollo-adminservice
+cp ./apollo-adminservice/target/apollo-adminservice-2.2.0-github.zip ./docker_build/apollo-adminservice/apollo-adminservice/
+cd docker_build/apollo-adminservice/apollo-adminservice/
+unzip ./apollo-adminservice-2.2.0-github.zip 
+rm -f ./apollo-adminservice-2.2.0-github.zip
+cd ..
+docker build . -t apollo-adminservice-kingbase8-pg:2.2.0
+
+cd ..
+cd ..
+
+mkdir ./docker_build/apollo-configservice/apollo-configservice
+cp ./apollo-configservice/target/apollo-configservice-2.2.0-github.zip ./docker_build/apollo-configservice/apollo-configservice/
+cd docker_build/apollo-configservice/apollo-configservice/
+unzip ./apollo-configservice-2.2.0-github.zip 
+rm -f ./apollo-configservice-2.2.0-github.zip
+cd ..
+docker build . -t apollo-configservice-kingbase8-pg:2.2.0
+
+cd ..
+cd ..
+
+
+mkdir ./docker_build/apollo-portal/apollo-portal
+cp ./apollo-portal/target/apollo-portal-2.2.0-github.zip ./docker_build/apollo-portal/apollo-portal/
+cd docker_build/apollo-portal/apollo-portal/
+unzip ./apollo-portal-2.2.0-github.zip 
+rm -f ./apollo-portal-2.2.0-github.zip
+cd ..
+docker build . -t apollo-portal-kingbase8-pg:2.2.0
+
+cd ..
+cd ..
+
+mkdir image-tar
+cd image-tar
+docker save -o ./apollo-portal-kingbase8-pg.tar apollo-portal-kingbase8-pg:2.2.0  
+docker save -o ./apollo-configservice-kingbase8-pg.tar apollo-configservice-kingbase8-pg:2.2.0  
+docker save -o ./apollo-adminservice-kingbase8-pg.tar apollo-adminservice-kingbase8-pg:2.2.0  
+
+
+docker run -p 8090:8090 \
+    -e SPRING_DATASOURCE_URL="jdbc:kingbase8://192.168.1.176:54321/apolloconfigdb" \
+    -e SPRING_DATASOURCE_USERNAME=system -e SPRING_DATASOURCE_PASSWORD=Tomtaw001 \
+    -d -v /tmp/logs:/opt/logs --name apollo-adminservice apollo-adminservice-kingbase8-pg:2.2.0
+ 
+docker run -p 8080:8080 \
+    -e SPRING_DATASOURCE_URL="jdbc:kingbase8://192.168.1.176:54321/apolloconfigdb" \
+    -e SPRING_DATASOURCE_USERNAME=system -e SPRING_DATASOURCE_PASSWORD=Tomtaw001 \
+    -d -v /tmp/logs:/opt/logs --name apollo-configservice apollo-configservice-kingbase8-pg:2.2.0
+	
+ 
+docker run -p 8070:8070 \
+    -e SPRING_DATASOURCE_URL="jdbc:kingbase8://192.168.1.176:54321/apolloportaldb" \
+    -e SPRING_DATASOURCE_USERNAME=system -e SPRING_DATASOURCE_PASSWORD=Tomtaw001 \
+    -e APOLLO_PORTAL_ENVS=dev,pro \
+    -e DEV_META=http://192.168.1.152:8080 -e PRO_META=http://192.168.1.152:8080 \
+    -d -v /tmp/logs:/opt/logs --name apollo-portal apollo-portal-kingbase8-pg:2.2.0
